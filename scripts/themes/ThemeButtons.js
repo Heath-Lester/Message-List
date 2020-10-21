a856a63b68bfef2013e78af046ab8f009abbd1a5
Heath Lester <h.allen.lester@gmail.com>
2020-10-21 10:38:23 -0500

    Broke id recognition for buttons

--- scripts/themes/ThemeButtons.js
+++ scripts/themes/ThemeButtons.js
@@ -2,9 +2,9 @@ const contentTarget = document.querySelector(".themes")
 
 export const ThemeButtons = () => {
     contentTarget.innerHTML = `
-        <button class="themeButton" id="themeButton--red">Red</button>
-        <button class="themeButton" id="themeButton--purple">Purple</button>
-        <button class="themeButton" id="themeButton--blue">Blue</button>
-        <button class="themeButton" id="themeButton--green">Green</button>
+        <button class="themeButton" id="btnTheme--red">Red</button>
+        <button class="themeButton" id="btnTheme--purple">Purple</button>
+        <button class="themeButton" id="btnTheme--blue">Blue</button>
+        <button class="themeButton" id="btnTheme--green">Green</button>
     `
 }
